584422397582a15d4b37e879cbf7555fbf4690d7
Alexis Garcia Saez <lexisgasa@uoc.edu>
2025-02-27 22:51:07 +0100

    Click enter to focus - useRef

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,4 +1,22 @@
+import { useEffect, useRef } from "react";
+
 const SearchBar = ({ query, setQuery }) => {
+  const inputElement = useRef(null);
+
+  useEffect(() => {
+    const callback = (event) => {
+      if (document.activeElement === inputElement.current) return;
+
+      if (event.code === "Enter") {
+        inputElement.current.focus();
+        setQuery("");
+      }
+    };
+
+    document.addEventListener("keydown", callback);
+    return () => document.addEventListener("keydown", callback);
+  }, [setQuery]);
+
   return (
     <input
       className="search"
@@ -6,6 +24,7 @@ const SearchBar = ({ query, setQuery }) => {
       placeholder="Type a movie title here.."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputElement}
     />
   );
 };
